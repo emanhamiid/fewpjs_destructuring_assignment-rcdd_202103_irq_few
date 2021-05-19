c3230daf4ae44ae26ee1b45c87baf82b1e39cac9
eman hamid <emanhharki@gmail.com>
2021-05-19 21:28:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,13 @@ const nestedMuppet = {
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
 
+const  {'cow', 'horse', 'sheep','pig','chicken'} = ("chai");
+
+
+
+
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
+
 
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
 
@@ -39,7 +45,7 @@ const nestedMuppet = {
 
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
 
-// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
+// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg.
 
 // Objects
 
